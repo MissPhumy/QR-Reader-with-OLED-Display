86df571f9762fd6279602978e93a2d410ae94a47
Prudence Radebe <122433264+MissPhumy@users.noreply.github.com>
2024-05-02 19:36:00 +0200

    Create Qr_generator.py

---
+++ Qr_generator.py
@@ -0,0 +1,20 @@
+import qrcode
+
+def generate_qr_code(data, file_name):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(file_name)
+
+if __name__ == "__main__":
+    data = input("Enter data to encode in QR code: ")
+    file_name = input("samples.png ")
+
+    generate_qr_code(data, file_name)
